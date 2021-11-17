4e80e55866c7a922341cf04f8aaa77eff2c43f85
Olivier Cartier <me@cestoliv.com>
2021-11-17 22:16:26 +0100

    un peu plus clean

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ocartier <ocartier@student.42lyon.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 10:04:59 by ocartier          #+#    #+#             */
-/*   Updated: 2021/11/17 22:08:07 by ocartier         ###   ########.fr       */
+/*   Updated: 2021/11/17 22:16:10 by ocartier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,98 +33,33 @@ char	*get_next_line(int fd)
 	char			*buf;
 	int				cur;
 	char			*readed;
-	//int				is_end;
-	char			*tmp;
 	static char		*previous;
 
 	if (!BUFFER_SIZE || BUFFER_SIZE < 1 || fd < 0 || read(fd, 0, 0) < 0)
 		return (NULL);	
-	readed = init_empty_string();
-	//is_end = 0;
 	if (!previous)
-	{
-		previous = init_empty_string();
-		if (!previous)
+		if (!(previous = init_empty_string()))
 			return (NULL);
-	}
-	/*
-	else
-	{
-		long	nlindex = charchr(previous, 10);
-		//printf("nl : %zu\n", nlindex);
-		if (nlindex >= 0)
-		{
-			tmp = ft_substr(previous, 0, nlindex + 1);
-			readed = ft_strjoin(readed, tmp);
-			previous = ft_substr(previous, nlindex + 1, ft_strlen(previous) + 1);
-			free(tmp);
-			if (ft_strlen(readed) == 0)
-			{
-				//free(readed);
-				return (NULL);
-			}
-			return (readed);
-		}
-		//printf("previous : %s\n", previous);
-		//return (NULL);
-	}
-	*/
-	readed = ft_strjoin(readed, previous);
-	if (!readed)
-		return (NULL);
-	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buf)
+	if (!(buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char))))
 		return (NULL);
 	while (charchr(previous, '\n') < 0 && (ret = read(fd, buf, BUFFER_SIZE)))
 	{
 		buf[ret] = 0;
 		previous = ft_strjoin(previous, buf);
 	}
+	free(buf);
 	cur = charchr(previous, '\n');
 	if (cur >= 0)
 	{
 		readed = ft_substr(previous, 0, cur + 1);
-		//previous = ft_substr(previous, cur + 1, ft_strlen(previous));
-		shiftstr(&previous, cur + 1);
+		previous = ft_substr(previous, cur + 1, ft_strlen(previous));
+		//shiftstr(&previous, cur + 1);
 	}
 	else
 	{
 		readed = ft_substr(previous, 0, ft_strlen(previous));
 		free(previous);
 	}
-	//printf("prev : %s, read = %s, cur = %d\n", previous, readed, cur);
-	//printf("prev : %s", previous);
-	//ret = read(fd, buf, BUFFER_SIZE);
-	/*
-	while ((ret = read(fd, buf, BUFFER_SIZE)) && !is_end)
-	{
-		buf[ret] = 0;
-		cur = charchr(buf, '\n');	
-		if (cur >= 0)
-		{
-			previous = ft_substr(buf, cur + 1, ret);
-			buf = ft_substr(buf, 0, cur + 1);
-			if (!previous || !buf)
-				return (NULL);
-			is_end = 1;
-		}
-		readed = ft_strjoin(readed, buf);
-		if (!readed)
-			return (NULL);
-//		if (!is_end)
-			//ret = read(fd, buf, BUFFER_SIZE);
-	}
-	free(buf);
-	*/
-	/*
-	if (!ret)
-	{
-		//free(previous);
-		previous = NULL;
-		//printf("free2");
-	}
-	*/
-	free(buf);
 	if (ft_strlen(readed) == 0)
 	{
 		free(readed);
